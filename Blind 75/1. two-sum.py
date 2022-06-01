4010483f7f318eb3bee3f949a2c3529ac1768cb0
Darryl Tanzil <darryltanzil@gmail.com>
2022-06-01 11:35:59 -0700

    explanation for two-sum

--- Blind 75/1. two-sum.py
+++ Blind 75/1. two-sum.py
@@ -2,10 +2,17 @@
 # remember to explain initial brute force solution
 
 So, my first approach to this problem would be to think of a brute force way of doing it.
-Given an array of integers nuts, and an integer target, we have to find the two numbers that add up to the target, that are not the same. Well the first thing I would think of Is to loop through every item in the array, and then for every item, loop through it again, add the two numbers together, and see if it equals to target. If it does, return the indices of the two. If not, then it would fail.
+Given an array of integers nuts, and an integer target, we have to find the two numbers that add up to the target, 
+that are not the same. Well the first thing I would think of Is to loop through every item in the array, and then for every item,
+loop through it again, add the two numbers together, and see if it equals to target. 
+If it does, return the indices of the two. If not, then it would fail.
 
-This initial solution would have a time complexity of n^2, since it loops through it twice. In order to fix this, we have to think of an alternative solution. 
-One way of doing so is to find a number, when added to the current indice, equals the target- we then have to find that target in the array. We can do so by creating a hash map, with the array value as the key, and the index as the hash value.
+This initial solution would have a time complexity of n^2, since it loops through it twice.
+In order to fix this, we have to think of an alternative solution. 
+One way of doing so is to find a number, when added to the current indice, equals the target- we then have to find that target in the array. 
+We can do so by creating a hash map, with the array value as the key, and the index as the hash value.
+We would then determine what the value is that would need to be added to the current index, and then find the index of it. If it doesn't exist,
+keep going. If at the very end, there is nothing, return an empty array.
 
 
 class Solution:
