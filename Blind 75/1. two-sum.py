f95ba84911904b638fa3acf15728d9b3d40e207c
Darryl Tanzil <darryltanzil@gmail.com>
2022-06-01 11:34:24 -0700

    Update 1. two-sum.py

--- Blind 75/1. two-sum.py
+++ Blind 75/1. two-sum.py
@@ -1,6 +1,13 @@
 # hash table solution
 # remember to explain initial brute force solution
 
+So, my first approach to this problem would be to think of a brute force way of doing it.
+Given an array of integers nuts, and an integer target, we have to find the two numbers that add up to the target, that are not the same. Well the first thing I would think of Is to loop through every item in the array, and then for every item, loop through it again, add the two numbers together, and see if it equals to target. If it does, return the indices of the two. If not, then it would fail.
+
+This initial solution would have a time complexity of n^2, since it loops through it twice. In order to fix this, we have to think of an alternative solution. 
+One way of doing so is to find a number, when added to the current indice, equals the target- we then have to find that target in the array. We can do so by creating a hash map, with the array value as the key, and the index as the hash value.
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash = {}
@@ -23,4 +30,3 @@ class Solution:
                 return [seen[remaining], i]
             seen[v] = i 
         return []
-            
